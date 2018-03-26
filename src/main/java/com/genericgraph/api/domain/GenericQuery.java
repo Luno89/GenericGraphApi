f3aae1109f10d0164c48e13ea433dfaa43583448
Corigan Simpson <corigan.simpson@gmail.com>
2018-03-26 11:15:54 -0700

    Refactoring the query building

--- src/main/java/com/genericgraph/api/domain/GenericQuery.java
+++ src/main/java/com/genericgraph/api/domain/GenericQuery.java
@@ -6,4 +6,49 @@ public class GenericQuery {
     public ArrayList<String> labels;
     public ArrayList<QueryParameter> nodes;
     public ArrayList<QueryParameter> relationships;
+
+    public static class Builder {
+        final String queryStart = "MATCH (n) WHERE ";
+        final String queryEnd = "RETURN n";
+        public String labels = "";
+        public String values = "";
+        public ArrayList<QueryParameter> relationships;
+        GenericQuery genericQuery;
+
+        public Builder(GenericQuery genericQuery) {
+            this.genericQuery = genericQuery;
+        }
+
+        Builder withLabels(ArrayList<String> labels) {
+            if (labels != null && labels.size() > 0) {
+                StringBuilder labelStringBuilder = new StringBuilder();
+                labels.forEach(s -> {
+                    labelStringBuilder.append(":" + s);
+                });
+                this.labels = "n" + labelStringBuilder.toString() + " ";
+            }
+            return this;
+        }
+
+        Builder withValues(ArrayList<QueryParameter> parameters) {
+            if (parameters != null) {
+                StringBuilder valuesString = new StringBuilder();
+                parameters.forEach((QueryParameter p) -> {
+                    valuesString.append(p.toString() + " AND ");
+                });
+                if (valuesString.length() > 4) {
+                    values = valuesString.toString().substring(0, valuesString.length()-4);
+                }
+            }
+
+            return this;
+        }
+
+        public String build() {
+            withLabels(this.genericQuery.labels);
+            withValues(this.genericQuery.nodes);
+            String where = values != "" && labels != "" ? values + "AND " + labels : values + labels;
+            return queryStart + where + queryEnd;
+        }
+    }
 }
---
+++ src/test/groovy/com/genericgraph/api/domain/GenericQuerySpec.groovy
@@ -0,0 +1,23 @@
+package com.genericgraph.api;
+
+import spock.lang.Specification
+import com.genericgraph.api.domain.*;
+
+class GenericQuerySpec extends Specification {
+
+    def "Can build query with labels" () {
+        when:
+        String result = new GenericQuery.Builder(new GenericQuery(labels:['furry', 'otaku'])).build()
+
+        then:
+        result == 'MATCH (n) WHERE n:furry:otaku RETURN n'
+    }
+
+    def "Can build query with values" () {
+        when:
+        String result = new GenericQuery.Builder(new GenericQuery(nodes:[new QueryParameter('name','=','zach')])).build()
+
+        then:
+        result == "MATCH (n) WHERE n.name = 'zach' RETURN n"
+    }
+}
